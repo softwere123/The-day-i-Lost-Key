e43e73522fe819ca7c16d264a2846af8dfb9f9ab
Myungseo++ <aa6053bb@gmail.com>
2025-05-13 18:14:40 +0900

    [Feat] 마우스 호버시 색깔 변경 코드
    단일책임패턴 연구중

--- The-day-i-Lost-Key/Assets/The Day i Lost Key/Scripts/CamerFuntuon/SelectionManager.cs
+++ The-day-i-Lost-Key/Assets/The Day i Lost Key/Scripts/CamerFuntuon/SelectionManager.cs
@@ -1,18 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+//code summation : while pracacties single-Responsibility principle  end yet
 
+// two tpye of default and highlight share one by one will make much easier to manage
 //단일 책임 원칙 각 책임을 가져와 별도의 인터 페이스로 옮겨 선택관리자가 한가지만 책임짐으로 한다
 public class SelectionManager : MonoBehaviour
 {
     [SerializeField] private string selectableTag = "Selectable"; // 선택 가능한 오브젝트의 태그
     [SerializeField] private Material highlightMaterial;
     [SerializeField] private Material defaultMaterial; // 기본 재질
- 
 
+    private HighlightSelectionResponse _selecltionResponse;
 
     private Transform _selection;
 
@@ -22,14 +25,12 @@ public class SelectionManager : MonoBehaviour
         // Deselection/Selection Response
         if (_selection != null)
         {
-            var selection = _selection;
-            var selectionRenderer = _selection.GetComponent<Renderer>();
-            if (selectionRenderer != null)
-            {
-                selectionRenderer.material = defaultMaterial;
-            }
+          
+            _selecltionResponse.OnDeselect(_selection);
+            //컨트롤 . 으로 함수 간소화 
         }
         #region Raycasting and Selection
+        //이거 왜 28저거 가독성 하나로 저렇게 토글화?
 
         //Creating Ray
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -49,13 +50,38 @@ public class SelectionManager : MonoBehaviour
         // Deselection/Selection Response
         if (_selection != null)
         {
-            var selectionRenderer = _selection.GetComponent<Renderer>();
-            if (selectionRenderer != null) 
-            {
-                selectionRenderer.material = highlightMaterial;
-            }
+
+            _selecltionResponse.OnSelect(_selection);
+            //Extra method 사용하여 기존 코드에서 클래스를 분리화 하여 옮길수 있게 하였다
         }
 
     }
+
+  
+
+  
+}
+internal class HighlightSelectionResponse : MonoBehaviour
+{
+    [SerializeField] public Material highlightMaterial;
+    [SerializeField] public Material defaultMaterial; // 기본 재질
+    private void OnSelect(Transform selection)
+    {
+        var selectionRenderer = _selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = _selecltionResponse.highlightMaterial;
+        }
+    }
+
+    private void OnDeselect()
+    {
+        var selectionRenderer = _selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = _selecltionResponse.defaultMaterial;
+        }
+    }
+
 
 }
