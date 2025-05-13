0e0976b1efe3f5f7619b16ae1e648361ab9186c0
Myungseo++ <aa6053bb@gmail.com>
2025-05-13 18:16:10 +0900

    [chore] 에셋 이동 삭제 프리팹 생성
    [chore] 에셋 이동 삭제 프리팹 생성

---
+++ The-day-i-Lost-Key/Assets/The Day i Lost Key/Scripts/CamerFuntuon/HighlightSelectionResponse1.cs
@@ -0,0 +1,27 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+internal class HighlightSelectionResponse : MonoBehaviour
+{
+    [SerializeField] public Material highlightMaterial;
+    [SerializeField] public Material defaultMaterial; // 기본 재질
+    private void OnSelect(Transform selection)
+    {
+        var selectionRenderer = _selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = _selecltionResponse.highlightMaterial;
+        }
+    }
+
+    private void OnDeselect()
+    {
+        var selectionRenderer = _selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = _selecltionResponse.defaultMaterial;
+        }
+    }
+
+
+}
